aca1e584254489d4b07915226d78f0271a83c401
Chris-Bortel <bortelcc19@gmail.com>
2020-06-20 14:57:51 -0700

    class

--- class-06/demo/final/server/server.js
+++ class-06/demo/final/server/server.js
@@ -1,50 +1,50 @@
-'use strict';
+"use strict";
 
 // Load Environment Variables from the .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // Application Dependencies
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // Application Setup
 const PORT = process.env.PORT;
 const app = express();
+
 app.use(cors());
 
-app.get('/', (request, response) => {
-  response.send('Home Page!');
+app.get("/", (req, res) => {
+  res.send("Home Page!");
 });
 
-app.get('/bad', (request, response) => {
-  throw new Error('poo');
+app.get("/bad", (request, res) => {
+  throw new Error("poo");
 });
 
 // The callback can be a separate function. Really makes things readable
-app.get('/about', aboutUsHandler);
+app.get("/about", aboutUsHandler);
 
 function aboutUsHandler(request, response) {
-  response.status(200).send('About Us Page');
+  response.status(200).send("About Us Page");
 }
 
 // API Routes
-app.get('/location', handleLocation);
-app.get('/restaurants', handleRestaurants);
+app.get("/location", handleLocation);
+app.get("/restaurants", handleRestaurants);
 
-app.use('*', notFoundHandler);
+app.use("*", notFoundHandler);
 app.use(errorHandler);
 
 // HELPER FUNCTIONS
 
 function handleLocation(request, response) {
   try {
-    const geoData = require('./data/location.json');
+    const geoData = require("./data/location.json");
     const city = request.query.city;
     const locationData = new Location(city, geoData);
     response.send(locationData);
-  }
-  catch (error) {
-    errorHandler('So sorry, something went wrong.', request, response);
+  } catch (error) {
+    errorHandler("So sorry, something went wrong.", request, response);
   }
 }
 
@@ -57,15 +57,14 @@ function Location(city, geoData) {
 
 function handleRestaurants(request, response) {
   try {
-    const data = require('./data/restaurants.json');
+    const data = require("./data/restaurants.json");
     const restaurantData = [];
-    data.nearby_restaurants.forEach(entry => {
+    data.nearby_restaurants.forEach((entry) => {
       restaurantData.push(new Restaurant(entry));
     });
     response.send(restaurantData);
-  }
-  catch (error) {
-    errorHandler('So sorry, something went wrong.', request, response);
+  } catch (error) {
+    errorHandler("So sorry, something went wrong.", request, response);
   }
 }
 
@@ -76,14 +75,12 @@ function Restaurant(entry) {
 }
 
 function notFoundHandler(request, response) {
-  response.status(404).send('huh?');
+  response.status(404).send("huh?");
 }
 
 function errorHandler(error, request, response) {
   response.status(500).send(error);
 }
-
-
 
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
